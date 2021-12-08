020a453c5c577dc1fc1aa31382fff415735412d5
Abdullah Ayyousha <abdo.5e4a@gmail.com>
2021-12-08 08:11:41 +0200

    provider state

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'modules/bmi/bmi_screen.dart';
+import 'providers/providers.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MultiProvider(
+        providers: [
+        ChangeNotifierProvider<providerChange>(create: (context) => providerChange(),)
+        ],
+      child: MyApp(),
+    )
+  );
 }
 
 class MyApp extends StatelessWidget {
